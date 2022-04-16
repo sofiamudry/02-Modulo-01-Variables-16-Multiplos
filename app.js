69d2ec0dc76cdd5643ce8b7564e43e9b5df78fe7
Sofia Mudry <sofiamudry@MBP-de-Sofi.fibertel.com.ar>
2022-04-16 16:20:29 -0300

    First commit

---
+++ app.js
@@ -0,0 +1,13 @@
+console.log('multiplos');
+
+alert('Multiplos');
+
+const numero01 = prompt('Ingrese el primer numero');
+const numero02 = prompt('Ingrese el segundo numero');
+const resto = Number(numero01) % Number(numero02);
+
+if (resto == "0") {
+       alert("Es multiplo");
+    } else {
+        alert("No es multiplo");
+    }
